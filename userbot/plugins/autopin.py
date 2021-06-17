c3b1d6f340fcacb5ba93f9b8fa798735d3f1c841
azuwlaa <64716619+azuwlaa@users.noreply.github.com>
2021-06-18 02:10:51 +0500

    Update autopin.py

--- userbot/plugins/autopin.py
+++ userbot/plugins/autopin.py
@@ -1,1 +1,12 @@
-Import 
+import asyncio
+
+from pyrogram import filters
+from pyrogram.methods.chats.get_chat_members import Filters as ChatMemberFilters
+from pyrogram.types import Message, ChatPermissions
+
+from userbot import UserBot
+from userbot.plugins.help import add_command_help
+
+
+
+@UserBot.on_message(filters.command("pin", ".") & filters.me)

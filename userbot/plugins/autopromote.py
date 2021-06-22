ec970e8d87a1fec69435031ba1bd593ae1651990
azuwlaa <64716619+azuwlaa@users.noreply.github.com>
2021-06-22 15:34:45 +0500

    Update autopromote.py

--- userbot/plugins/autopromote.py
+++ userbot/plugins/autopromote.py
@@ -19,8 +19,8 @@ async def auto_pro_admin(_, message: Message):
 
         await UserBot.send_message(chat_id=chat_id,
                                    text=text, reply_to_message_id=message.message_id)
-        await asyncio.sleep(1)
-        await message.delete()
+            await asyncio.sleep(1)
+            await message.delete()
 
                   
 @UserBot.on_message(filters.regex(r"(?i)demoteme(.*)$"))
@@ -31,6 +31,6 @@ async def auto_demote_admin(_, message: Message):
 
         await UserBot.send_message(chat_id=chat_id,
                                    text=text, reply_to_message_id=message.message_id)
-        await asyncio.sleep(1)
-        await message.delete()
+            await asyncio.sleep(1)
+            await message.delete()
                 

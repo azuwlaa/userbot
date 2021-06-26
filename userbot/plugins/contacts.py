133bc4874bc9e40f4bde6cc94c1a0a27be84e1e8
azuwlaa <64716619+azuwlaa@users.noreply.github.com>
2021-06-26 23:41:04 +0500

    Update contacts.py

--- userbot/plugins/contacts.py
+++ userbot/plugins/contacts.py
@@ -10,5 +10,5 @@ from userbot.helpers.PyroHelpers import GetChatID
 async def un_block(_, message: Message):
             text = "`Unblocked!`"
 
-            await UserBot.send_message(chat_id=chat_id,
-                                   text=text, reply_to_message_id=message.message_id)
+            await UserBot.send_message(text=text, 
+                                       reply_to_message_id=message.message_id)

4523b61a1927eb7d5dd3c199fd1d89fae4083457
azuwlaa <64716619+azuwlaa@users.noreply.github.com>
2021-06-26 23:38:21 +0500

    Update contacts.py

--- userbot/plugins/contacts.py
+++ userbot/plugins/contacts.py
@@ -6,12 +6,10 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 
-user_first = message.reply_to_message.from_user.first_name
-
-
 @UserBot.on_message(filters.command("unblock", ".") & filters.me)
 async def un_block(_, message: Message):
-        await add_contact("added", reply_to_message_id=user_first)
-        await message.edit(
-                "`UNBLOCKED!`"
-            )
+        if chat_id = GetChatID(message) 
+            text = "`Unblocked!`"
+
+            await UserBot.send_message(chat_id=chat_id,
+                                   text=text, reply_to_message_id=message.message_id)

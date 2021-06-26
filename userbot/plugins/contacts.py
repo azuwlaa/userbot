ef736f07fcaadb350c0b1a34a56b4b569100997f
azuwlaa <64716619+azuwlaa@users.noreply.github.com>
2021-06-26 23:19:08 +0500

    Update contacts.py

--- userbot/plugins/contacts.py
+++ userbot/plugins/contacts.py
@@ -7,12 +7,6 @@ from pyrogram.types import Message
 @UserBot.on_message(filters.command("unblock", ".") & filters.me)
 async def un_block(_, message: Message):
     user_first = message.reply_to_message.from_user.first_name
-        if message.reply_to_message is None:
-        await message.edit(
-                "`WHO SHOULD I UNBLOCK?`"
-            )
-        return
-    else:
         await add_contact("username", "Bar")
         await message.edit(
                 "`UNBLOCKED!`"
